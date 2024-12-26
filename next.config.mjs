717fd5211e93dd372f4ab141552a2f77493a88bb
최영빈 <bin778@naver.com>
2024-12-26 15:54:38 +0900

    feat: 환경변수 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
+  },
+};
 
 export default nextConfig;
