78ad86b45d62eb04611b3958d6067db5d8223d58
최영빈 <bin778@naver.com>
2024-12-30 16:26:11 +0900

    refactor: 헤더 매개변수 분리

--- src/app/api/[route]/route.js
+++ src/app/api/[route]/route.js
@@ -1,21 +1,14 @@
 import axios from 'axios';
 import { ERROR_MESSAGE } from '@/constant/error.js';
-import { jsonHeaders } from '@/constant/json.js';
-
-const NAVER_API_URL = 'https://openapi.naver.com/v1/search/news.json';
-
-const HEADERS = {
-  'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
-  'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
-};
+import { jsonHeaders, apiURL, headers } from '@/constant/headers.js';
 
 export const GET = async (req) => {
   const query = new URL(req.url).searchParams.get('query') || '뉴스';
 
   try {
-    const { data } = await axios.get(NAVER_API_URL, {
+    const { data } = await axios.get(apiURL, {
       params: { query, display: 100, start: 1 },
-      headers: HEADERS,
+      headers: headers,
     });
     return new Response(JSON.stringify(data), { status: 200, headers: jsonHeaders });
   } catch (error) {
---
+++ src/constant/headers.js
@@ -0,0 +1,6 @@
+export const jsonHeaders = { 'Content-Type': 'application/json' };
+export const headers = {
+  'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+  'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+};
+export const apiURL = 'https://openapi.naver.com/v1/search/news.json';
